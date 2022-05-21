3a8455db17d616926adfd8885de00a269cfa96b6
Gustavo Pelayo <76456590+gustavopelayo@users.noreply.github.com>
2022-05-21 17:30:12 +0100

    Dashboard Users

--- vehicles/urls.py
+++ vehicles/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import AddVehicleView, UserVehicleListView,  UserVehicleDetailView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from . import views
 
 urlpatterns = [
     path('vehicles/', UserVehicleListView.as_view(), name='vehicle-list'),
-    path('vehicles/<int:pk>/',  UserVehicleDetailView.as_view() , name='vehicle-detail'),
+    path('vehicles/<int:pk>/',  views.UserVehicleDetailView , name='vehicle-detail'),
     path('vehicles/add/', AddVehicleView.as_view() , name='vehicle-form'),
 
 ]
--- vehicles/views.py
+++ vehicles/views.py
@@ -1,7 +1,8 @@
-from ssl import VERIFY_CRL_CHECK_CHAIN
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Vehicle
 from blog.models import Post
-from vehicles import forms
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
@@ -11,8 +12,14 @@ from django.views.generic import (
 from django.contrib.auth.models import User
 
 from django.shortcuts import render, get_object_or_404
-from django.contrib import messages
 from vehicles.forms import VehicleAddForm
+import plotly.express as px
+import plotly.offline as plot
+import plotly.graph_objects as go
+from datetime import datetime
+import plotly.express as px
+
+
 
 class UserVehicleListView(ListView):
     model = Vehicle
@@ -22,18 +29,38 @@ class UserVehicleListView(ListView):
     def get_queryset(self):
         print(Vehicle.objects.filter(owner_id= self.request.user.id))
         return Vehicle.objects.filter(owner_id= self.request.user.id)
-class UserVehicleDetailView(ListView):
-    model = Post
-    template_name = 'vehicles/vehicle_detail.html'  # <app>/<model>_<viewtype>.html
+
+def UserVehicleDetailView(request, pk):
+    
+    
+    posts = Post.objects.filter(vehicle=pk)
+    dates = []
+    powers = []
+    times = []
+    chargers = []
+    for post in posts:
+        dates.append(post.hourin)
+        powers.append(post.power)
+        chargers.append(post.chargers.nickname)
+    for date in dates:
+        date = date.strftime('%H:%M:%S')
+        times.append(date)
+
     context_object_name = 'posts'
+    print(times)
+    print(powers)
+    print(chargers)
+    template = loader.get_template('vehicles/vehicle_detail.html')
+    data = [go.Scatter(x = times, y = powers,name='Net Worth')]
+    graph = plot.plot(data, auto_open=False, output_type='div')
+    data2=[go.Pie(labels=chargers)]
+    fig = plot.plot(data2, auto_open=False, output_type='div')
 
+    context = {'graph': graph, 'posts': posts, 'fig': fig}
+    print(posts)
+    return render(request, 'vehicles/vehicle_detail.html', context = {'graph': graph, 'posts': posts, 'fig':fig })
 
-    def get_queryset(self):
-
-        vehicle = get_object_or_404(Vehicle, id =self.kwargs.get('pk'))
-        
-        print(Post.objects.filter(vehicle= vehicle))
-        return Post.objects.filter(vehicle= vehicle)
+    
 
 class AddVehicleView(LoginRequiredMixin, CreateView):
     
